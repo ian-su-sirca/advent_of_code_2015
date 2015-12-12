6f149bf62f562ebfdf337ca4298a472500499c0c
Ian Su <ian.su@sirca.org.au>
2015-12-12 15:43:24 +1100

    Day 3

--- day03/part1.py
+++ day03/part1.py
@@ -3,10 +3,30 @@ from common import *
 
 class DayProcessor(Processor):
     def before(self):
-        pass
+        self.coords = {}
+        self.x = 0
+        self.y = 0
+    def move_dir(self, move):
+        if move == '^':
+            self.y -= 1
+        elif move == 'v':
+            self.y += 1
+        elif move == '<':
+            self.x -= 1
+        elif move == '>':
+            self.x += 1
+        else:
+            raise ValueError(move)
+        coord = '(%d,%d)' % (self.x, self.y)
+        if coord not in self.coords:
+            self.coords[coord] = 1
+        else:
+            self.coords[coord] += 1
     def process_line(self, line):
+        for move in line:
+            self.move_dir(move)
         pass
     def after(self):
-        pass
+        print len(self.coords.keys())
 
 DayProcessor().run()
--- day03/part2.py
+++ day03/part2.py
@@ -1,12 +1,46 @@
 #!/usr/bin/env python
 from common import *
 
+class Mover(object):
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+    def move(self, move):
+        if move == '^':
+            self.y -= 1
+        elif move == 'v':
+            self.y += 1
+        elif move == '<':
+            self.x -= 1
+        elif move == '>':
+            self.x += 1
+        else:
+            raise ValueError(move)
+    def coord(self):
+        return '(%d,%d)' % (self.x, self.y)
+
 class DayProcessor(Processor):
     def before(self):
-        pass
+        self.coords = {}
+        self.santa = Mover()
+        self.robot = Mover()
+    def move_dir(self, move, isSanta):
+        if isSanta:
+            mover = self.santa
+        else:
+            mover = self.robot
+        mover.move(move)
+        coord = mover.coord()
+        if coord not in self.coords:
+            self.coords[coord] = 1
+        else:
+            self.coords[coord] += 1
     def process_line(self, line):
-        pass
+        isSanta = True
+        for move in line:
+            self.move_dir(move, isSanta)
+            isSanta = not isSanta
     def after(self):
-        pass
+        print len(self.coords.keys())
 
 DayProcessor().run()
